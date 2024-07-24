0c48ea962e4256a6b1ed7b605cca858b71bcc03f
Muh11 <naufalaswinana@gmail.com>
2024-07-24 22:09:45 +0700

    feat: model for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,31 +16,33 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+    'first_name', 
+    'last_name', 
+    'email', 
+    'instagram', 
+    'linkedin', 
+    'batch_no', 
+    'hashed_password', 
+    'email_verified_at'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function staff()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasOne(Staff::class);
+    }
+
+    public function supervisor()
+    {
+        return $this->hasOne(Supervisor::class);
+    }
+
+    public function cLevel()
+    {
+        return $this->hasOne(CLevel::class);
+    }
+
+    public function monthlyFeedback()
+    {
+        return $this->hasMany(MonthlyFeedback::class);
     }
 }

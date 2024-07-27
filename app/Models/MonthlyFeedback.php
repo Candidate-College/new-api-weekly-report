10c4cb5720f4926ce3ea8483520353a38e7755e0
KronosDP <darrel.danadyaksa19@gmail.com>
2024-07-27 11:36:43 +0700

    fix: monthly feedback model

--- app/Models/MonthlyFeedback.php
+++ app/Models/MonthlyFeedback.php
@@ -2,31 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MonthlyFeedback extends Model
 {
-    use HasFactory;
+    protected $fillable = ['user_id', 'year', 'month', 'content_text'];
 
-    protected $table = 'monthly_feedbacks';
-    
-    protected $fillable = [
-        'user_id',
-        'year',
-        'month',
-        'content_text',
-    ];
+    protected $primaryKey = ['user_id', 'year', 'month'];
+    public $incrementing = false;
 
-    public function user(): BelongsTo
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
-
-    public function supervisor(): BelongsTo
-    {
-        return $this->belongsTo(User::class, 'supervisor_id');
-    }
 }
-

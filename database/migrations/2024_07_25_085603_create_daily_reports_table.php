34e90e93b0be07cdd436439f386cd915e7a689fd
KronosDP <darrel.danadyaksa19@gmail.com>
2024-07-27 11:33:49 +0700

    feat: fix daily reports table

--- database/migrations/2024_07_25_085603_create_daily_reports_table.php
+++ database/migrations/2024_07_25_085603_create_daily_reports_table.php
@@ -4,27 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDailyReportsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('daily_reports', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamp('created_at');
             $table->text('content_text');
             $table->string('content_photo')->nullable();
-            $table->timestamps();
+            $table->timestamp('last_updated_at');
+
+            $table->primary(['user_id', 'created_at']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('daily_reports');
     }
-};
+}

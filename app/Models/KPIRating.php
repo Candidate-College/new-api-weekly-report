f77aeea8cc1cd8dda18bd4ba624203956a40a528
KronosDP <darrel.danadyaksa19@gmail.com>
2024-07-27 11:36:33 +0700

    fix: KPI rating model

--- app/Models/KPIRating.php
+++ app/Models/KPIRating.php
@@ -2,19 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class KPIRating extends Model
 {
-    use HasFactory;
-    
-    protected $table = 'kpi_ratings';
+    protected $fillable = [
+        'user_id', 'year', 'month', 'activeness_Q1_score', 'activeness_Q2_score',
+        'activeness_Q3_score', 'ability_Q1_score', 'communication_Q1_score',
+        'communication_Q2_score', 'discipline_Q1_score', 'discipline_Q2_score',
+        'discipline_Q3_score',
+    ];
 
-    protected $fillable = ['user_id', 'year', 'month', 'activeness_Q1_score', 'activeness_Q2_score', 'activeness_Q3_score', 'ability_Q1_score', 'communication_Q1_score', 'communication_Q2_score', 'discipline_Q1_score', 'discipline_Q2_score', 'discipline_Q3_score'];
+    protected $primaryKey = ['user_id', 'year', 'month'];
+    public $incrementing = false;
 
-    public function user(): BelongsTo
+    public function user()
     {
         return $this->belongsTo(User::class);
     }

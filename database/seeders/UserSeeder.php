b3902e7c6d2a0ddb406ce4acbaf13eef5c711a10
Muh11 <naufalaswinana@gmail.com>
2024-07-28 00:41:38 +0700

    fix: create 110 user with relation each role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,45 +8,28 @@ use Illuminate\Database\Seeder;
 class UserSeeder extends Seeder
 {
     public function run()
-    {
-        // Create a C-level user (6 c-level users and it's vice c-level)
-        User::factory()->count(12 * 2)->create([
-            'CFlag' => true,
-            'Sflag' => false,
-            'StFlag' => false,
-        ]);
-
-        // Create some supervisors (18 subdivision with 2 supervisors with 1.3 more load for future expansion)
-        User::factory()->count(ceil(18 * 2 * 1.3))->create([
+{
+   
+    $clevels = User::factory()->count(10)->clevel()->create();
+    $supervisors = User::factory()->count(20)->supervisor($clevels->random())->create();
+    $supervisors->each(function ($supervisor) use ($clevels) {
+        $supervisor->update([
+            'supervisor_id' => $clevels->random()->id,
+            'vice_supervisor_id' => $clevels->random()->id,
             'CFlag' => false,
             'Sflag' => true,
             'StFlag' => false,
         ]);
+    });
 
-        // Create regular staff (100 staff with 1.3 more load for future expansion)
-        User::factory()->count(ceil(100 * 1.3))->create([
+    $staffs = User::factory()->count(80)->create();
+    $staffs->each(function ($staff) use ($supervisors) {
+        $staff->update([
+            'supervisor_id' => $supervisors->random()->id,
+            'vice_supervisor_id' => $supervisors->random()->id,
             'CFlag' => false,
             'Sflag' => false,
             'StFlag' => true,
         ]);
-
-        // Assign supervisors and vice supervisors for staff
-        $staff = User::where('StFlag', true)->get();
-        $supervisors = User::where('Sflag', true)->get();
-        $cLevel = User::where('CFlag', true)->first();
-
-        // Assign supervisors and vice supervisors for staff
-        $staff->each(function ($user) use ($supervisors) {
-            $user->supervisor()->associate($supervisors->random());
-            $user->viceSupervisor()->associate($supervisors->random());
-            $user->save();
-        });
-
-        // Assign c-level and vice c-level for supervisors
-        $supervisors->each(function ($supervisor) use ($cLevel) {
-            $supervisor->supervisor()->associate($cLevel->random());
-            $supervisor->viceSupervisor()->associate($cLevel->random());
-            $supervisor->save();
-        });
-    }
-}
+    });
+}}

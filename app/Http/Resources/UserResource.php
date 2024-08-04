704cf75bbd2d82759b84f70a7e5262d5bcff7ad9
Muh11 <naufalaswinana@gmail.com>
2024-08-05 00:13:54 +0700

    feat: create Resource and UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'instagram' => $this->instagram,
+            'linkedin' => $this->linkedin,
+            'batch_no' => $this->batch_no,
+            'division' => $this->division,
+            'supervisor_id' => $this->supervisor_id,
+            'vice_supervisor_id' => $this->vice_supervisor_id,
+            'CFlag' => $this->CFlag,
+            'Sflag' => $this->Sflag,
+            'StFlag' => $this->StFlag,
+            'profile_picture' => $this->profile_picture,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}

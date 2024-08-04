bfb61617fb076303e44e258ac6eeacb7f082b596
Taufik Hidayat <taufikhdyt@student.ub.ac.id>
2024-08-04 20:25:56 +0700

    feat: group API routes by functionality with appropriate middleware

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -7,7 +7,22 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
 Route::prefix('v1')->group(function () {
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index']);
+
+    // Route Auth
+    Route::prefix('auth')->group(function () {
+        // 
+    });
+
+    // Route Report
+    Route::prefix('reports')->middleware('auth:api')->group(function () {
+        // 
+    });
+
+    // Route Feedback
+    Route::prefix('feedback')->middleware('auth:api')->group(function () {
+        // 
+    });
+
 });

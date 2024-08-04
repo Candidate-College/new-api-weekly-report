63e1cac0c3f78f00adf2d76607032b1d2bf4a3f5
Muh11 <naufalaswinana@gmail.com>
2024-08-04 18:16:38 +0700

    feat: create route with prefix v1/auth for JWT authentication

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -2,12 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
+
+
 Route::prefix('v1')->group(function () {
+    Route::group([
+        'prefix' => 'auth'
+    ], function ($router) {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);
+        
+        Route::group(['middleware' => 'auth:api'], function($router) {
+            Route::post('/logout', [AuthController::class, 'logout']);
+            Route::post('/refresh', [AuthController::class, 'refresh']);
+            Route::get('/user-profile', [AuthController::class, 'userProfile']);
+        });
+    });
+
+
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index']);
 });

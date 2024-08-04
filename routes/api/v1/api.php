21027a678e3ea199a3570cd642205e9ef9c4b6be
Muh11 <naufalaswinana@gmail.com>
2024-08-05 00:14:42 +0700

    feat: add show user

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -9,5 +10,6 @@ Route::get('/user', function (Request $request) {
 
 
 Route::prefix('v1')->group(function () {
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index']);
+    Route::get('users', [UserController::class, 'index']);
+    Route::get('users/{id}', [UserController::class, 'show']);
 });

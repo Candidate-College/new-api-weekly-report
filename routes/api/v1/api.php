67f24df969da6126c3ab79dfb27b744381df111a
Muh11 <naufalaswinana@gmail.com>
2024-08-05 11:44:35 +0700

    fix: adjust route for further routing
    - Adjust for further grouping endpoint

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -3,21 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-
-
 Route::prefix('v1')->group(function () {
-    Route::group([
-        'prefix' => 'auth'
-    ], function ($router) {
-        Route::post('/login', [AuthController::class, 'login']);
+    Route::get('users', [UserController::class, 'index']);
+
+
+    // Route Auth
+    Route::group(['prefix' => 'auth'], function ($router) {
         Route::post('/register', [AuthController::class, 'register']);
-        
+        Route::post('/login', [AuthController::class, 'login']);
+
         Route::group(['middleware' => 'auth:api'], function($router) {
             Route::post('/logout', [AuthController::class, 'logout']);
             Route::post('/refresh', [AuthController::class, 'refresh']);
@@ -26,5 +29,5 @@ Route::prefix('v1')->group(function () {
     });
 
 
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index']);
+
 });

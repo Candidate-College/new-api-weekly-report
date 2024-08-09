16f82df61616c372d1b83d358824206dea7722ea
Muh11 <naufalaswinana@gmail.com>
2024-08-09 22:37:45 +0700

    feat: create divisions seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            DivisionSeeder::class,
             UserSeeder::class,
             MonthlyFeedbackSeeder::class,
             OTPSeeder::class,

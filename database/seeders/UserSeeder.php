a9ed8702a315b295b2c9b6d34a9205929bc6746c
bebeorca <dimasrdyo@gmail.com>
2024-11-26 18:34:37 +0700

    add register@docs

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Division;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,5 +15,6 @@ class UserSeeder extends Seeder
         \DB::unprepared(
             file_get_contents('.\database\db_new_api_weekly_report.sql')
         );
+        // User::factory(10)->create();
     }
 }
